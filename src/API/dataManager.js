1f6b452959553e0446fac0541bf14754a77d5f45
chturner94 <49048780+chturner94@users.noreply.github.com>
2023-03-18 22:39:54 -0400

    All Datamanagement files are working.

--- src/API/dataManager.js
+++ src/API/dataManager.js
@@ -1,4 +1,4 @@
-import { FILES } from "../constants/constants.js";
+import {FILES} from "../constants/constants.js";
 
 
 export const listFiles = async (targetDirectory, sourceDirectory, depositArray) =>
@@ -11,6 +11,23 @@ export const listFiles = async (targetDirectory, sourceDirectory, depositArray) 
             }
          });
       };
+export const doesFileExist = async (sourceDirectory, fileName) =>
+{
+   const fileArray = [];
+   await listFiles(FILES.ORIGIN_FOLDER, `${FILES.moduleDataDirectory}`, fileArray);
+      console.log(fileArray);
+      console.log(`${sourceDirectory}/${fileName}`);
+   if (fileArray.includes(`${sourceDirectory}${fileName}`))
+   {
+      console.log('file exists');
+      return true;
+   }
+   else
+   {
+      console.log('file does not exist');
+      return false;
+   }
+};
 
 export async function fetchFile(sourceDirectory, fileName)
 {
@@ -43,34 +60,6 @@ export const createUploadFolder = async () =>
    }
 };
 
-export function findCurrentWorldIDIndex()
-{
-   let index = 0;
-   const worldName = game.world.id;
-   console.log(worldName);
-
-   fetchFile(FILES.moduleDataDirectory, FILES.backupManagerJSON).then((contents) =>
-   {
-      const backupManagerObject = contents;
-      console.log(backupManagerObject);
-      while (index < backupManagerObject.data.length)
-      {
-         if (backupManagerObject.data[index].world === worldName)
-         {
-            console.log(index);
-            return index;
-         }
-         else
-         {
-            console.log('incrementing');
-            index++;
-            console.log(index);
-         }
-      }
-   });
-   console.log(index);
-   return index;
-}
 // create a backup file
 
 export const createInitBackupStore = async () =>
@@ -79,35 +68,24 @@ export const createInitBackupStore = async () =>
    {
       const findFiles = [];
       await listFiles(FILES.ORIGIN_FOLDER, `${FILES.moduleDataDirectory}/`, findFiles);
-      const detectBackupManager = findFiles.includes(`${FILES.moduleDataDirectory}/${FILES.backupManagerJSON}`);
+      console.log(findFiles);
       const emptyFile = {
          name: "this is a test to see if overwrite occurs"
       };
-      const backupManagementFile = {
-         name: "backupManager",
-         data: [
-            {
-               world: "",
-               storeIndex: 0,
-            }
-         ]
-      };
-      let index = 1;
-      if (!detectBackupManager)
-      {
-         const backupManagementJSON = new File([JSON.stringify(backupManagementFile)], FILES.backupManagerJSON);
-         await FilePicker.upload(FILES.ORIGIN_FOLDER, `${FILES.moduleDataDirectory}/`, backupManagementJSON);
-         console.log("backup management file created");
-      }
-      while (index < 5)
-      {
          const worldName = game.world.id;
-         const backUpFileString = `${worldName}-${index}.json`;
-         const backupFile = new File([JSON.stringify(emptyFile)], backUpFileString, { type: 'application/json' });
-         await FilePicker.upload(FILES.ORIGIN_FOLDER, `${FILES.moduleDataDirectory}/`, backupFile);
-         console.log(backupFile);
-         index++;
-      }
+         const fileExist = await doesFileExist(`${FILES.moduleDataDirectory}/`, `${worldName}.json`);
+         console.log(fileExist);
+         if (!fileExist)
+         {
+            const backUpFileString = `${worldName}.json`;
+            const backupFile = new File([JSON.stringify(emptyFile)], backUpFileString, { type: 'application/json' });
+            await FilePicker.upload(FILES.ORIGIN_FOLDER, `${FILES.moduleDataDirectory}/`, backupFile);
+            return console.log(`Initializing backup for ${worldName}`);
+         }
+         else
+         {
+            return console.log(`${worldName} has already been initialized`);
+         }
    }
    catch (e)
    {
@@ -118,57 +96,25 @@ export const createInitBackupStore = async () =>
 const createBackupFile = async (data) =>
 {
    const worldName = game.world.id;
-   const index = findCurrentWorldIDIndex();
-
-   let storeIndex = fetchFile(`${FILES.moduleDataDirectory}`, FILES.backupManagerJSON).then((contents) =>
-   {
-      return contents.data[index].storeIndex++;
-   });
-   if (storeIndex > 5)
-   {
-      storeIndex = 1;
-   }
-   const backupFileString = `${worldName}-${storeIndex}.json`;
+   const backupFileString = `${worldName}.json`;
    return new File([JSON.stringify(data)], backupFileString, { type: 'application/json' });
 };
 export const POSTBackup = async (data) =>
 {
-
    const backupFile = await createBackupFile(data);
    await FilePicker.upload(FILES.ORIGIN_FOLDER, `${FILES.moduleDataDirectory}/`, backupFile);
-   fetchFile(FILES.moduleDataDirectory, FILES.backupManagerJSON).then((contents) =>
-   {
-      const backupManagerObject = contents;
-      const backupManagerIndex = findCurrentWorldIDIndex();
-      let storeIndex = backupManagerObject.data[backupManagerIndex].storeIndex++;
-      if (storeIndex > 5)
-      {
-         storeIndex = 1;
-      }
-      backupManagerObject.data[backupManagerIndex].storeIndex = storeIndex;
-      const newBackupManagerFile = new File([JSON.stringify(backupManagerObject)], FILES.backupManagerJSON, { type: 'application/json' });
-      FilePicker.upload(FILES.ORIGIN_FOLDER, `${FILES.moduleDataDirectory}/`, newBackupManagerFile);
-   });
 };
 
-   export function fetchIndexedBackupFiles()
+   export function fetchBackupFile(localDataObject)
    {
       const worldName = game.world.id;
-      const worldIndex = findCurrentWorldIDIndex();
-      const backupManagerObject = fetchFile(FILES.moduleDataDirectory, FILES.backupManagerJSON).then((contents) =>
-      {
-         return contents;
-      });
-      console.log(backupManagerObject);
-      const index = backupManagerObject
-      console.log(index);
-
-      const backupFileString = `${worldName}-${index}.json`;
+      const backupFileString = `${worldName}.json`;
       console.log(backupFileString);
-      const backupFile = fetchFile(FILES.moduleDataDirectory, backupFileString);
-      const parseJSON = JSON.parse(backupFile);
-      console.log(parseJSON);
-      return parseJSON;
+      localDataObject =  fetchFile(FILES.moduleDataDirectory, backupFileString).then((data) =>
+      {
+         return data;
+      });
+      return localDataObject;
    }
 
 
--- src/view/GMJournal/GMJournal.svelte
+++ src/view/GMJournal/GMJournal.svelte
@@ -1,24 +1,16 @@
 <script>
    import { ApplicationShell }   from '@typhonjs-fvtt/runtime/svelte/component/core';
-   import { grabFile } from "./GMJournal.js";
    import {
       POSTBackup,
       createInitBackupStore,
-      fetchFile,
-      fetchIndexedBackupFiles, findCurrentWorldIDIndex
+      fetchBackupFile
    } from "../../API/dataManager.js";
 
    export let elementRoot;
-let testBackupData
-let backupData
-function updateBackupData() {
-   if(testBackupData !== undefined) {
-      console.log("setting Test Data to backupData")
-      testBackupData = backupData
-   } else {
-      console.log("testBackupData is undefined")
-   }
-}
+   const dataForTesting = {
+      "test": "test",
+      "test2": "test2"
+   };
 
 </script>
 
@@ -34,13 +26,8 @@ function updateBackupData() {
       <button on:click={() => createInitBackupStore()}>Initialize Stores</button>
       <!-- text field for creating a data value to pass into back -->
       <!-- <input id="textBox" type="text" bind:value={cutomBackupData} /> -->
-      <button on:click={() => fetchFile(FILES.moduleDataDirectory, "backup-manager.json")}>Fetch File</button>
-      <button on:click={() => fetchIndexedBackupFiles()}>Fetch Backup</button>
-      <h2>{testBackupData}</h2>
-      <input id="textBox" type="text" bind:value={testBackupData} />
-      <input id="submit" type="submit" value="Submit" on:click={() => updateBackupData} />
-      <button on:click={() => POSTBackup(backupData)}>Create Backup</button>
-      <button on:click={() => findCurrentWorldIDIndex()}>Find ID</button>
+      <button on:click={() => fetchBackupFile()}>Fetch Backup</button>
+      <button on:click={() => POSTBackup(dataForTesting)}>CreateBackup</button>
 
    </main>
 </ApplicationShell>
