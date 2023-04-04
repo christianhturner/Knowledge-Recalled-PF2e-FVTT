5b35b0b0bd3e1a0fb9657581409268c7b9a6c6f6
cturnrq <cturnrq@amazon.com>
2023-04-03 20:02:28 -0400

    Requires testing but added the requirements for the sub-type marker.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import GMJournalApplication from "./view/GMJournal/GMJournal.js";
 import { createUploadFolder } from "./API/dataManager.js";
 import { getConstants } from "./constants/constants.js";
 import {getNPCActorFromEncounters} from "./control/Actor.js";
+import KRActor from "./models/knowledgeRecalledDataModel.js";
+
+Hooks.on("init", () => {
+   Object.assign(CONFIG.Actor.dataModels, {
+      KRNPC: KRActor
+   });
+});
 
 Hooks.once('ready', getConstants());
 Hooks.once('ready', () => new GMJournalApplication().render(true, { focus: true }));
