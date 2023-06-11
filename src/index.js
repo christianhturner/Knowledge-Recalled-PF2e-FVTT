a77ec9d13bcd6e4b2ce04239d3ee00434de6baed
AlecHolloway <rh1397@msstate.edu>
2023-06-11 16:00:15 -0500

    Fixed updateNPCModelFlags. checkForChangesOnUpdate(actor) function was indirectly triggering the update hook due to the call to this.actor.setFlag(). Each time the flags were updated, it triggered an update to the actor, which in turn called the updateNPCModelFlags(actor) function, creating an infinite loop.

--- src/index.js
+++ src/index.js
@@ -44,6 +44,15 @@ Hooks.on('createActor', (actor, options, userId) =>
 
    }
 });
+Hooks.on("updateActor", async (actor, updateData) => {
+   // Check if the update is relevant to the NPC flags
+   console.log("Updating Actor");
+   if (updateData?.flags?.["fvtt-knowledge-recalled-pf2e"]?.npcFlags) {
+      // Call the function to update the NPC model flags
+      await updateNPCModelFlags(actor);
+   }
+});
+
 
 async function initNPCModel(actor)
 {
@@ -59,61 +68,87 @@ async function initNPCModel(actor)
       console.error("Error initializing NPCModel: ", error);
    }
 }
+// Function to update the NPC model flags
 async function updateNPCModelFlags(actor)
 {
    try
    {
-      const KRNPC = await new NPCModel(actor);
-      await KRNPC.checkForChangesOnUpdate(actor);
-      KRNPC.processValues();
-      console.log("Knowledge Recalled NPC: ", KRNPC);
+      const existingFlags = actor.getFlag("fvtt-knowledge-recalled-pf2e", "npcFlags");
+      if (existingFlags)
+      {
+         // Exclude visibility properties from the existingFlags object
+         const updatedFlags = Object.entries(existingFlags).reduce((flags, [key, value]) =>
+         {
+            if (!key.endsWith(".visibility"))
+            {
+               flags[key] = value;
+            }
+            return flags;
+         }, {});
+
+         // Repopulate the values that have path declarations
+         updatedFlags.baseCharacterInfo.name = actor.name;
+         updatedFlags.baseCharacterInfo.creatureType = actor.system.details.creatureType;
+         updatedFlags.baseCharacterInfo.alliance = actor.alliance;
+         updatedFlags.baseCharacterInfo.actorImg = actor.img;
+         updatedFlags.baseCharacterInfo.description = actor.description;
+         updatedFlags.rarity.value = actor.rarity;
+         updatedFlags.privateInfo.privateDescription = actor.system.details.privateNotes;
+         updatedFlags.privateInfo.CR = actor.level;
+
+         updatedFlags.armorClass.value = actor.attributes.ac.base;
+         updatedFlags.fortSave.value = actor.saves.fortitude.dc.value;
+         updatedFlags.refSave.value = actor.saves.reflex.dc.value;
+         updatedFlags.willSave.value = actor.saves.will.dc.value;
+         // Repopulate other values as needed...
+
+         await actor.setFlag("fvtt-knowledge-recalled-pf2e", "npcFlags", updatedFlags);
+         console.log("Flags updated:", updatedFlags);
+      }
+      else
+      {
+         console.log("No existing flags found. Initializing flags...");
+         // Initialize flags if necessary
+         // ...
+      }
    }
    catch (error)
    {
-      console.error("Error initializing NPCModel: ", error);
-   }
-
-
-   /*Hooks.on('updateActor', (actor, options, userId) =>
-   {
-      // Check if the actor is an NPC
-      if (actor.type === 'npc')
-      {
-         updateNPCModelFlags(actor).then((r) => console.log(r));
-      }
-   });*/
-
-
-   function getActiveEncounters()
-   {
-      const encounters = game.combats.combats;
-      let activeEncounters = [];
-      activeEncounters = encounters.filter((encounter) => encounter.active === true);
-      if (!activeEncounters)
-      {
-         console.log("No active encounter found.");
-         return [];
-      }
-      return activeEncounters;
-   }
-
-   async function addNPCtoGlobalArray(encounter)
-   {
-      const npcCombatants = encounter.turns;
-      npcCombatants.forEach((actor) =>
-      {
-         if (
-          !npcActors.find((npcActor) =>
-          {
-             return npcActor.actorId === actor.actorId;
-          }) &&
-          actor.isNPC === true
-         )
-         {
-            const newActor = game.actors.get(actor.actorId);
-            npcActors.push(newActor);
-         }
-      });
+      console.error("Error updating NPC flags:", error);
    }
 }
+
+
+function getActiveEncounters()
+{
+   const encounters = game.combats.combats;
+   let activeEncounters = [];
+   activeEncounters = encounters.filter((encounter) => encounter.active === true);
+   if (!activeEncounters)
+   {
+      console.log("No active encounter found.");
+      return [];
+   }
+   return activeEncounters;
+}
+
+async function addNPCtoGlobalArray(encounter)
+{
+   const npcCombatants = encounter.turns;
+   npcCombatants.forEach((actor) =>
+   {
+      if (
+       !npcActors.find((npcActor) =>
+       {
+          return npcActor.actorId === actor.actorId;
+       }) &&
+       actor.isNPC === true
+      )
+      {
+         const newActor = game.actors.get(actor.actorId);
+         npcActors.push(newActor);
+      }
+   });
+}
+
 
