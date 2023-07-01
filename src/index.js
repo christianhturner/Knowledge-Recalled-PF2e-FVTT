34d51d95eaeffbc071f3c0764a330abfb3ef09e3
AlecHolloway <rh1397@msstate.edu>
2023-07-01 10:35:58 -0500

    Working on Model

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ import { isEqual } from 'lodash';
 
 
 
-console.log("loading knowledge recalled");
-const npcActors = [];
+//const npcActors = [];
+
+KnowledgeRecalled._onReady();
 
 // Remove for production
 const isDev = true;
@@ -24,56 +25,93 @@ Hooks.once('ready', () => new GMJournalApplication().render(true, { focus: true 
 
 Hooks.on("ready", () =>
 {
-   console.log("KnowledgeRecalled Activity ");
-   const activeEncounters = getActiveEncounters();
-   console.log("activeEncounters: ", activeEncounters);
-   console.log("npcActors: ", npcActors);
-   for (const element of activeEncounters)
-   {
-      addNPCtoGlobalArray(element);
-   }
-   KnowledgeRecalled._onReady(npcActors);
-   const KnowledgeRecalledActors = KnowledgeRecalled.getActors();
-   console.log("KnowledgeRecalledActors: ", KnowledgeRecalledActors);
+   getActiveEncounters();
+
 
 });
+
+function getActiveEncounters()
+{
+   const encounters = game.combats.combats;
+   let activeEncounters = [];
+   activeEncounters = encounters.filter((encounter) => encounter.active === true);
+   if (!activeEncounters)
+   {
+      console.log("No active encounter found.");
+      return [];
+   }
+
+   for (const element of activeEncounters)
+   {
+      addNPCtoKnowledgeRecalledEncounters(element);
+   }
+
+}
+
+/**
+ *
+ */
+function addNPCtoKnowledgeRecalledEncounters(encounter)
+{
+   const npcCombatants = encounter.turns;
+   npcCombatants.forEach((actor) => 
+   {
+      const newActor = game.actors.get(actor.actorId);
+      ui.KnowledgeRecalled.addToEncounteredActorArray(newActor);
+   });
+}
 
 Hooks.on('createActor', (actor, options, userId) =>
 {
    // Check if the actor is an NPC
    if (actor.type === 'npc')
    {
-      console.log('begin initNPCModel');
-
       initNPCModel(actor).then((r) => console.log(r));
-
-      console.log('end initNPCModel');
    }
 });
-Hooks.on("updateActor", async (actor, updateData) => {
-   // Check if the update is relevant to the NPC flags
-   console.log("updateData", updateData);
-   console.log("actor", actor);
 
-   if (updateData)
+/**
+ *
+ */
+function initNPCModel(actor)
+{
+   try
    {
-      // Call the function to update the NPC model flags
-      await updateNPCModelFlags(actor, updateData);
+      const KnowledgeRecalledNPCActors = new NPCModel(actor);
+      KnowledgeRecalledNPCActors.processValues();
+      ui.KnowledgeRecalled.addToNPCActorArray(KnowledgeRecalledNPCActors);
+   }
+   catch (error)
+   {
+      console.error("Error initializing NPCModel: ", error);
+   }
+}
+
+Hooks.on("updateActor", async (actor, updatedData) =>
+{
+   // Check if the update is relevant to the NPC flags
+   if (updatedData)
+   {
+      await updateNPCModelFlags(actor, updatedData);
    }
 });
 
 // Function to update the NPC model flags
-async function updateNPCModelFlags(actor, updateData) {
+async function updateNPCModelFlags(actor, updatedData)
+{
    // Access the updated flags from the updateData object
-   console.log("updateData", updateData);
 
-   try {
+   try
+   {
       const existingFlags = actor.getFlag("fvtt-knowledge-recalled-pf2e", "npcFlags");
-      if (existingFlags) {
+      if (existingFlags)
+      {
          // Exclude visibility properties from the existingFlags object
 
-         const updatedFlags = Object.entries(existingFlags).reduce((flags, [key, value]) => {
-            if (!key.endsWith(".visibility")) {
+         const updatedFlags = Object.entries(existingFlags).reduce((flags, [key, value]) =>
+         {
+            if (!key.endsWith(".visibility"))
+            {
                flags[key] = value;
             }
             return flags;
@@ -95,69 +133,29 @@ async function updateNPCModelFlags(actor, updateData) {
          updatedFlags.willSave.value = actor.saves.will.dc.value;
          // Repopulate other values as needed...
 
-         if (!isEqual(existingFlags, updatedFlags)) {
+         if (!isEqual(existingFlags, updatedFlags))
+         {
             await actor.setFlag("fvtt-knowledge-recalled-pf2e", "npcFlags", updatedFlags);
             console.log("Flags updated:", updatedFlags);
-         } else {
+         }
+         else
+         {
             console.log("Flags have not changed. Skipping update.");
          }
-      } else {
+      }
+      else
+      {
          console.log("No existing flags found. Initializing flags...");
          // Initialize flags if necessary
          // ...
       }
-   } catch (error) {
+   }
+   catch (error)
+   {
       console.error("Error updating NPC flags:", error);
    }
 }
 
 
-
-async function initNPCModel(actor)
-{
-   try
-   {
-      const KRNPC = new NPCModel(actor);
-      console.log("Knowledge Recalled NPC: ", KRNPC);
-      KRNPC.processValues();
-      console.log("Knowledge Recalled NPC: ", KRNPC);
-   }
-   catch (error)
-   {
-      console.error("Error initializing NPCModel: ", error);
-   }
-}
-
-function getActiveEncounters()
-{
-   const encounters = game.combats.combats;
-   let activeEncounters = [];
-   activeEncounters = encounters.filter((encounter) => encounter.active === true);
-   if (!activeEncounters)
-   {
-      console.log("No active encounter found.");
-      return [];
-   }
-   return activeEncounters;
-}
-
-async function addNPCtoGlobalArray(encounter)
-{
-   const npcCombatants = encounter.turns;
-   npcCombatants.forEach((actor) =>
-   {
-      if (
-       !npcActors.find((npcActor) =>
-       {
-          return npcActor.actorId === actor.actorId;
-       }) &&
-       actor.isNPC === true
-      )
-      {
-         const newActor = game.actors.get(actor.actorId);
-         npcActors.push(newActor);
-      }
-   });
-}
 
 
--- src/models/KnowledgeRecalled.js
+++ src/models/KnowledgeRecalled.js
@@ -1,28 +1,55 @@
-import NPCActor from "./ActorModel.js";
-import NPCModel from "./NPCModel.js";
+//import NPCActor from "./ActorModel.js";
+//import NPCModel from "./NPCModel.js";
 
 // eslint-disable-next-line no-unused-vars
 export default class KnowledgeRecalled extends Application
 {
-   static NPCActors = [];
-   constructor(FoundryActors)
-   {
-      super();
-      console.log("Storing NPC Actors");
-
-      for (const element of FoundryActors)
-      {
-         KnowledgeRecalled.NPCActors.push(new NPCModel(element));
-      }
-   }
+   NPCActors = [];
+   encounteredNPCActors = [];
+   // constructor(FoundryActors)
+   // {
+   //    super();
+   //    console.log("Storing NPC Actors");
+   //
+   //    for (const element of FoundryActors)
+   //    {
+   //       KnowledgeRecalled.NPCActors.push(new NPCModel(element));
+   //    }
+   // }
 
    static _onReady(listOfFoundryActors)
    {
       ui.KnowledgeRecalled = new KnowledgeRecalled(listOfFoundryActors);
    }
 
-   static getActors()
+   getActors()
    {
       return this.NPCActors;
+   }
+
+   getEncounteredActors()
+   {
+      return this.encounteredNPCActors;
+   }
+   checkForDuplicateActor(actorId)
+   {
+      return this.NPCActors.some((npcActor) => npcActor.actorId === actorId);
+   }
+   addToEncounteredActorArray(NPCActor)
+   {
+      const isDuplicate = this.checkForDuplicateActor(NPCActor.actorId);
+      if (!isDuplicate)
+      {
+         this.encounteredNPCActors.push(NPCActor);
+      }
+   }
+
+   addToNPCActorArray(NPCActor)
+   {
+      const isDuplicate = this.checkForDuplicateActor(NPCActor.actorId);
+      if (!isDuplicate)
+      {
+         this.encounteredNPCActors.push(NPCActor);
+      }
    }
 }
