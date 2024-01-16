214b87c773c161d97fc4e47ebf8385babbbde6fe
Christian <christian@Chimera-Server>
2024-01-15 19:59:26 -0500

    Hook to attach to when actors are updated

--- src/control/KRHooks.js
+++ src/control/KRHooks.js
@@ -13,6 +13,15 @@ export default async function registerHooks() {
     Hooks.on('closeApplication', (app, html) => {
         console.log(`Here is the application`, app);
         console.log(`Here is the html`, html);
+    });
+    // Hook will be used for updating the data between actor document and the NPCModel flag store
+    Hooks.on('updateActor', (actor, changes) => {
+        if (!changes) {
+            console.debug("No changes occured.")
+        };
+
+
+        console.log("Knowledge Recalled Update Actor", actor, changes);
     })
 
 };
