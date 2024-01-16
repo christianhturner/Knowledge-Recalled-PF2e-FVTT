9880484995802974b97bdcb3262fdd0de39f56c2
Christian <christian@Chimera-Server>
2024-01-16 06:18:11 -0500

    TODO: Test this implementation

--- src/models/NPCModel.js
+++ src/models/NPCModel.js
@@ -1,5 +1,7 @@
 import { dcByLevel, rarityMap } from "../constants/constants"
 import { removeFlag } from "../control/data.js"
+// If this is the manager, it should be independent of any actor, but we can register actors? and maybew
+// look them up based on their actorID?
 
 export default class NPCManager {
     constructor() {
