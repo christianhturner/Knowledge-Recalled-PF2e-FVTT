ff39ec88c2775a8e2c4ce78a7890689d4b0e523d
Christian Turner <cturnrq@amazon.com>
2024-01-21 13:51:58 -0500

    implemented flags, and a method to update the values when they change
    function for getting property value of an object
    
    Added check if actor isn't registered, to register the actor in the module. Also removed the recursion that is occuring when flags aren't available

--- src/models/NPCModel.js
+++ src/models/NPCModel.js
@@ -1,6 +1,6 @@
 import { dcByLevel, rarityMap } from "../constants/constants"
 import { removeFlag } from "../control/data.js"
-import { getActor } from "../control/utilities";
+import { getActor, getProperty } from "../control/utilities";
 
 // If this is the manager, it should be independent of any actor, but we can register actors? and maybew
 // look them up based on their actorID?
@@ -31,9 +31,15 @@ export default class NPCManager {
         let actorId;
         if (typeof actorOrId === Actor) {
             actor = actorOrId;
-            actorId = actor.actorId;
+            actorId = actor.id;
+            if (!this.getActor(actorId)) {
+                this.registerActor(actor);
+            };
         }
         if (typeof actorOrId === "string") {
+            if (!this.getActor(actorId)) {
+                this.registerActor(actorOrId);
+            }
             actorId = actorOrId;
             actor = getActor(actorOrId);
         };
@@ -44,7 +50,51 @@ export default class NPCManager {
             return console.debug(`Actor flags are already initialized for ${actorId}`, actor);
         }
         const flags = {
-
+            actorID: actor.id,
+            defaultDC: 0,
+            modifiedDC: 0,
+            baseCharacterInfo: {
+                visibility: false,
+                discoveredBy: '',
+            },
+            rarity: {
+                visibility: false,
+                discoveredBy: '',
+            },
+            privateInfo: {
+                visibility: false,
+                discoveredBy: '',
+            },
+            traits: {},
+            armorClass: {
+                visibility: false,
+                discoveredBy: '',
+            },
+            fortSave: {
+                visibility: false,
+                discoveredBy: '',
+            },
+            willSave: {
+                visibility: false,
+                discoveredBy: '',
+            },
+            refSave: {
+                visibility: false,
+                discoveredBy: '',
+            },
+            lowestSave: {
+                visibility: false,
+                discoveredBy: '',
+            },
+            immunities: {},
+            resistances: {},
+            weaknesses: {},
+            passiveAbilities: {},
+            actionAbilities: {},
+            spellAbilities: {},
+            difficultyAdjustmentByPlayerId: {
+                adjustment: {},
+            }
         };
         this.setFlags(flags, actor);
 
@@ -76,7 +126,7 @@ export default class NPCManager {
         let actorId;
         if (typeof actorOrId === Actor) {
             actor = actorOrId;
-            actorId = actor.actorId;
+            actorId = actor.id;
         } else {
             actorId = actorOrId;
             actor = getActor(actorId);
@@ -98,22 +148,60 @@ export default class NPCManager {
 
         this.npcActors.set(actor.id, actor);
 
+    };
+
+    /**
+    * method for taking Actor object and changes diff, and updating the flags accordingly, intended to be attached
+    * to the updateActor hook, and returns both the actor and the diff.
+    * @method
+    * @param {Actor} actor - actor object, can resolve if string of id is passed
+    * @param {Object} diff - value passed by the updateActor hook as a diff of the changed values.
+    * @returns {void} Updates flags on object
+    */
+    updateDiff(actor, diff) {
+        if (typeof actor === 'string') {
+            try {
+                actor = getActor(actor);
+            } catch (error) {
+                return console.error(`Invalid actor or actorId`, error)
+            };
+        };
+        const actorId = actor.id;
+        for (let [key, value] of Object.entries(diff)) {
+            // If value is an object call recursively
+            if (typeof value === 'object') {
+                this.updateDiff(actor, value);
+            } else {
+                let flag = this.getFlags(actorId);
+                if (flag[key]) {
+                    flag.value = getProperty(actor, key);
+                }
+            }
+        }
+
+
     }
 
     getFlags(actorId) {
         const actor = getActor(actorId);
         const flags = actor.getFlag('fvtt-knowledge-recalled-pf2e', 'npcFlags');
         if (!flags) {
-            flags = this.initializeFlags(actor);
-            return flags;
+            console.debug('No flags initialized, please initialize this actor.')
         }
         else {
             return flags;
         }
     };
 
+    /**
+    * Method to set the flags on NPC Actor objects
+    * @method
+    * @param {Object} flags - flags listed in the initializeFlags methods
+    * @param {Actor} actor - Foundry Actor object.
+    */
     setFlags(flags, actor) {
-
+        actor.setFlag('fvtt-knowledge-recalled-pf2e', 'npcFlags', flags);
+        console.debug(`Set flags on ${actor.name}:`, flags, actor);
     }
 
     calculateDC() {
