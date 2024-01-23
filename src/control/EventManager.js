8bd235ab58b61e15612b7f775ab5d5016fab3a20
Christian Turner <cturnrq@amazon.com>
2024-01-22 21:36:17 -0500

    Seeing about implementing an event Manager

--- src/control/EventManager.js
+++ src/control/EventManager.js
@@ -1,33 +1,34 @@
-import KnowledgeRecalled from "../KnowledgeRecalled";
-import { insertKnowledgeRecalledbuttons } from "../foundryUiOverrides";
-import EncounterManager from "../models/EncounterModel";
-import NPCManager from "../models/NPCModel";
-import ViewManager from "./ViewManager";
-export default async function registerHooks() {
-    Hooks.on("ready", () => {
-        KnowledgeRecalled._onReady();
-        ViewManager._onReady();
-        ui.KnowledgeRecalled.ViewManager.init();
-        NPCManager._onReady();
-        EncounterManager._onReady();
-    });
-    Hooks.on('getSceneControlButtons', (controls) => {
-        insertKnowledgeRecalledbuttons(controls);
-    });
-    Hooks.on('closeApplication', (app, html) => {
-        console.log(`Here is the application`, app);
-        console.log(`Here is the html`, html);
-    });
-    // Hook will be used for updating the data between actor document and the NPCModel flag store
-    Hooks.on('updateActor', (actor, changes) => {
-        if (!changes) {
-            console.debug("No changes occured.")
-        };
+import registerHooks from "./foundryHooks";
 
 
-        console.log("Knowledge Recalled Update Actor", actor, changes);
-    })
+export default class EventManager {
+    constructor() {
+        if (!ui.KnowledgeRecalled.EventManager) {
+            ui.KnowledgeRecalled.EventManager = this;
+            this.events = {};
+        }
+    };
+    registerEvent(name) {
 
-};
+    }
 
+    subcribe(event, callback) {
+        if (!this.events[event]) {
+            this.events[event] = [];
+        }
+        this.events[event].push(callback);
+    }
 
+    publish(event, data) {
+        if (!this.events[event]) {
+            this.events[event].forEach(callback => callback(data));
+        }
+    }
+
+    unsubscribe(event, callback) {
+        if (this.events[event]) {
+            this.events[event] = this.events[event].filter(cb => cb !== callback);
+        }
+    }
+
+}
