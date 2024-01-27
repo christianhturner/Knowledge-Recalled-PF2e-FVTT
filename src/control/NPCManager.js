62c3c148545f95e810bf8aabafa9db8476e9b7d5
Christian Turner <cturnrq@amazon.com>
2024-01-27 14:21:58 -0500

    Removing the mapped property on the Manager, and i will instead store similar data on the View whose stat this will directly impact. This more or less facilitates between the data and view model

--- src/control/NPCManager.js
+++ src/control/NPCManager.js
@@ -4,7 +4,6 @@ export default class NPCManager {
     constructor() {
         if (!ui.KnowledgeRecalled.NPCManager) {
             ui.KnowledgeRecalled.NPCManager = this;
-            this.npcActors = new Map();
         }
         return ui.KnowledgeRecalled.NPCManager;
     }
@@ -27,48 +26,6 @@ export default class NPCManager {
     * @param {string | Actor } actorOrId
     * @returns void 
     */
-    registerActor(actorOrId) {
-        let actor;
-        let actorId;
-        if (typeof actorOrId === Actor) {
-            actor = actorOrId;
-            actorId = actor.id;
-        } else {
-            actorId = actorOrId;
-            actor = getActor(actorId);
-        };
-
-        if (!actor) {
-            return console.debug(`Knowledge Recalled: registerActor error, ${actorOrId} is not valid.
-            Ensure you passed either the actor object or actorId.
-            `, actorOrId)
-        };
-
-        if (!actor.type == 'npc') {
-            return console.log(`The actor provided is not an NPC`);
-        }
-
-        if (this.npcActors.has(actorId)) {
-            return console.log("Knowledge recalled: registerActor Notice, ${actor.name} is already registered.", actorOrId);
-        }
-
-        this.npcActors.set(actor.id, actor);
-
-    };
-    /**
-    * Method that returns an actor object that exist and is stored on the object.
-    * @method
-    * @param {string} actorId - ActorId for foundry Actor object
-    * @returns {Actor | undefined}
-    */
-    getActor(actorId) {
-        let actor;
-        if (!this.npcActors.get(actorId)) {
-            console.debug(`Knowledge Recalled: getActor can't find ${actorId}`);
-        }
-        actor = this.npcActors.get(actorId);
-        return actor;
-    };
 
     /**
      * Method for returning the implementation of the NPCModel
