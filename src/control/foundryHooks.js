e63b3b12e15aac550d98ea0f76d88f6b132fed7e
Christian Turner <cturnrq@amazon.com>
2024-02-01 20:04:55 -0500

    Creating user interface when module detects duplicate actor being added into the world

--- src/control/foundryHooks.js
+++ src/control/foundryHooks.js
@@ -4,6 +4,8 @@ import ViewManager from "./ViewManager";
 import EncounterManager from "./EncounterManager";
 import NPCManager from "./NPCManager";
 import NPCModel from "../models/NPCModel";
+import { checkForExistingActor } from "./utilities";
+import DuplicateActorPrompt from "../view/Dialogs/DuplicateActorPrompt";
 
 export default async function registerHooks() {
     Hooks.on("ready", () => {
@@ -21,6 +23,11 @@ export default async function registerHooks() {
         console.log(`Here is the application`, app);
         console.log(`Here is the html`, html);
     });
+
+    Hooks.on('createActor', (actor, changes, userId) => {
+        checkForExistingActor(actor);
+        new DuplicateActorPrompt(actor);
+    })
     // Hook will be used for updating the data between actor document and the NPCModel flag store
     Hooks.on('updateActor', (actor, changes) => {
         if (!changes) {
