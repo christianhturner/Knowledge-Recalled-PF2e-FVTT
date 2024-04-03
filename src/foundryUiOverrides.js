0424b0be22abd81d7bcf9e6a568ded6ababc0a9e
Christian Turner <cturnrq@amazon.com>
2024-04-02 20:03:39 -0400

    (bug) previous commit removed needed export, simplified and got working

--- src/foundryUiOverrides.js
+++ src/foundryUiOverrides.js
@@ -3,25 +3,23 @@ import { API } from "./API/api";
 import { CONSTANTS } from "./constants/constants";
 
 
-
 // https://github.com/kandashi/Custom-Token-Animations/blob/910e1c887ab99f96639f1376dcc8ebd2effb1a92/src/CTA.js#L632
 
 /**
- * Function to insert the modules journal buttons.
- * @function insertJournalbuttons
- * @param{SceneControl} - Foundry scene control buttons.
+ *
+ * @param {object} sceneControl - https://foundryvtt.com/api/interfaces/client.SceneControl.html
  */
 export function insertKnowledgeRecalledbuttons(sceneControl) {
-   const moduleData = game.modules.get(CONSTANTS.moduleId)
+   const moduleData = game.modules.get(CONSTANTS.moduleId);
    /** @type {import('../src/API/api').API} */
-   const Api = moduleData.public
-   sceneControl.find(x => x.name == 'notes').tools.push({
+   const Api = moduleData.public;
+   sceneControl.find((x) => x.name === 'notes').tools.push({
       icon: 'fas fa-book-open',
       name: 'KnowledgeRecalled',
       title: 'KnowledgeRecalled',
       visible: true,
       toggle: true,
-      onClick: () => { Api.viewManager.openCloseNegotiate("gmJournal") },
+      onClick: () => { Api.viewManager.openCloseNegotiate("gmJournal"); },
       button: true,
    });
 
