80a53f8ef5957de0e2baae7bf9bf2b35485b80b1
Christian Turner <cturnrq@amazon.com>
2024-04-16 05:49:56 -0400

    formatting

--- src/control/stores/Store.js
+++ src/control/stores/Store.js
@@ -1,33 +1,36 @@
 class Store {
-    constructor(store, data) {
-        this.store = store;
-        this.subscriptions = [];
-        this.data = data;
-        this.setup(data);
-    };
+   constructor(store, data) {
+      this.store = store;
+      this.subscriptions = [];
+      this.data = data;
+      this.setup(data);
+   }
 
-    setupStore(data) {
-        this.unsubscribe();
-        this.setupStore(data);
-        this.setupSubscription(data);
-    };
+   setupStore(data) {
+      this.unsubscribe();
+      this.setupStore(data);
+      this.setupSubscription(data);
+   }
 
-    setupSubscription() {
-        // Override with higher order implementation
-    };
+   setupSubscription() {
+      // Override with higher order implementation
+   }
 
-    /**
+   /**
     * A method to subscribe to a store property.
-    * @method subscribeTo
+    *
+    * @function subscribeTo
+    *
     * @param target - Target can be any property on the item.
+    *
     * @param callback {function} - callback is often some sort of a set up function
     */
-    subscribeTo(target, callback) {
-        this.subscriptions.push(target.subscribe(callback));
-    }
+   subscribeTo(target, callback) {
+      this.subscriptions.push(target.subscribe(callback));
+   }
 
-    unsubscribeAll() {
-        this.subscriptions.forEach(unsubscribe => unsubscribe());
-        this.subscriptions = [];
-    }
+   unsubscribeAll() {
+      this.subscriptions.forEach((unsubscribe) => unsubscribe());
+      this.subscriptions = [];
+   }
 }
