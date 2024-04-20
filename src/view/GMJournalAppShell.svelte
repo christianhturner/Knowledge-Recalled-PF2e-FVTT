3944929f0438dba695b706ce475423955b6b645e
Christian Turner <cturnrq@amazon.com>
2024-04-20 05:19:28 -0400

    testing GMJournal

--- src/view/GMJournalAppShell.svelte
+++ src/view/GMJournalAppShell.svelte
@@ -3,7 +3,45 @@
 
 <script>
    import { ApplicationShell } from "#runtime/svelte/component/core";
+   import { log } from "../lib/debugger";
+   import { EncounterManager } from "../control/EncounterManager";
+   import { NPCManager } from "../control/NPCManager";
+   import { TJSDocument } from "#runtime/svelte/store/fvtt/document";
    export let elementRoot;
+
+   /**
+    * @typedef Encounter
+    * @prop {number} round - The numerical round the encounter is on
+    *
+    * @prop {number} sort - FIXME: NOT SURE
+    *
+    * @prop {null|number} turn - the turn within the round the encounter is on
+    *
+    * @prop {object[]} turns - an array of actor documents
+    *
+    * @prop {object} combatant - actor document whose active turn it is
+    */
+
+   const encounterManager = new EncounterManager();
+   log.log(encounterManager.listCurrentEncounter()); //TODO: remove
+
+   /**
+    * @function
+    * @param {Encounter} encounter
+    * @returns {object[]} - TODO: May be an Array of Arrays
+    */
+   function actorsFromEncounter(encounter) {
+      const combatants = encounter.contents[0].turns; // TODO: optimize the encounter so that we can simply call
+      // encounter.turns
+      const npcManager = new NPCManager();
+      const npcArray = [];
+      combatants.forEach((combatants) => {
+         const npc = npcManager.createNPCObject(combatants.actorId);
+         npcArray.push(new TJSDocument(npc.actor)); // TODO: think through TJSDOCUMENT
+      });
+      return npcArray;
+   }
+   log.log(actorsFromEncounter(encounterManager.listAllEncounters()));
 </script>
 
 <!-- ApplicationShell provides the popOut / application shell frame, header bar, content areas -->
